9e52d33289d0e4e89cc394bacb92c25b9914bafe
Benjamin Oakes <hello@benjaminoakes.com>
2016-01-23 22:53:51 -0600

    Clarify syntax

--- lib/markdo/help_command.rb
+++ lib/markdo/help_command.rb
@@ -6,15 +6,15 @@ module Markdo
       @stderr.puts <<-EOF
 Markdown-based task manager.
 
-    add                   Add a task to the inbox.  (Set $MARKDO_ROOT and $MARKDO_INBOX.)
+    add "string"          Add a task to the inbox.  (Set $MARKDO_ROOT and $MARKDO_INBOX.)
     edit                  Edit $MARKDO_ROOT in $EDITOR.
     help, --help          Display this help text.
     overview              Get overview of starred, today's, and tomorrow's tasks.
-    tag                   Search *.md files for @tag.
+    tag "string'          Search *.md files for @tag.
     today                 Search *.md files for today's date.  (YYYY-MM-DD format.)
     tomorrow              Search *.md files for tomorrow's date.  (YYYY-MM-DD format.)
     star, starred         Search *.md files for @star.
-    query, q              Search *.md files for string.
+    query, q "string"     Search *.md files for string.
     version, --version    Display the version.
 EOF
 
