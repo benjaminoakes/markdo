6225ce20870ee64ddc64bf6e887a966b9948d85d
Benjamin Oakes <hello@benjaminoakes.com>
2016-09-25 22:33:59 -0500

    Refactor: Extract method (task_collection)

--- lib/markdo/commands/ics_command.rb
+++ lib/markdo/commands/ics_command.rb
@@ -6,12 +6,17 @@ require 'markdo/models/task_collection'
 module Markdo
   class IcsCommand < Command
     def run
-      lines = DataSource.new(@env).all_lines
-      task_collection = TaskCollection.new(lines)
       ics_exporter = IcsExporter.new(task_collection)
       ics = ics_exporter.to_ics
 
       @stdout.puts(ics)
+    end
+
+    private
+
+    def task_collection
+      lines = DataSource.new(@env).all_lines
+      TaskCollection.new(lines)
     end
   end
 end
