6ea7d528869c3d71dd9e357f6dc43b533a787199
Benjamin Oakes <hello@benjaminoakes.com>
2016-09-25 21:39:39 -0500

    Refactor: Extract class (IcsExporter)

--- lib/markdo/ics_command.rb
+++ lib/markdo/ics_command.rb
@@ -1,5 +1,6 @@
 require 'date'
 require 'markdo/command'
+require 'markdo/ics_exporter'
 require 'markdo/task_collection'
 
 module Markdo
@@ -10,59 +11,17 @@ module Markdo
         map { |path| File.readlines(path, encoding: 'UTF-8') }.
         flatten
 
-      events = TaskCollection.new(lines).
-        with_attribute('due').
-        reject { |task| task.complete? }.
-        map { |task|
-          due_date = task.attributes['due'].date_value
-          Event.new(due_date, due_date, clean(task.body))
-        }
-
-      ics = template(events)
+      task_collection = TaskCollection.new(lines)
+      ics_exporter = IcsExporter.new(task_collection)
+      ics = ics_exporter.to_ics
 
       @stdout.puts(ics)
     end
 
     protected
 
-    class Event
-      def initialize(date_start, date_end, summary)
-        @date_start = date_start
-        @date_end = date_end
-        @summary = summary
-      end
-
-      def to_ics
-        buf = []
-        buf << 'BEGIN:VEVENT'
-        buf << "DTSTART;VALUE=DATE:#{@date_start.strftime('%Y%m%d')}"
-        buf << "DTEND;VALUE=DATE:#{@date_end.strftime('%Y%m%d')}"
-        buf << "SUMMARY:#{@summary}"
-        buf << 'END:VEVENT'
-        buf.join("\n")
-      end
-    end
-
     def markdown_glob
       "#{@env['MARKDO_ROOT']}/*.md"
-    end
-
-    def clean(line)
-      line.sub(%r(@due\(\b\d{4}-\d{2}-\d{2}\b\)\s), '')
-    end
-
-    def template(events)
-      buf = []
-
-      buf << 'BEGIN:VCALENDAR'
-      buf << 'VERSION:2.0'
-      buf << 'CALSCALE:GREGORIAN'
-      buf << 'METHOD:PUBLISH'
-      buf << 'X-WR-CALNAME:Markdo Due Dates'
-      buf << events.map { |event| event.to_ics }
-      buf << 'END:VCALENDAR'
-
-      buf.flatten.join("\n")
     end
   end
 end
---
+++ lib/markdo/ics_exporter.rb
@@ -0,0 +1,55 @@
+module Markdo
+  class IcsExporter
+    def initialize(task_collection)
+      @task_collection = task_collection
+    end
+
+    def events
+      @task_collection.
+        with_attribute('due').
+        reject { |task| task.complete? }.
+        map { |task|
+          due_date = task.attributes['due'].date_value
+          Event.new(due_date, due_date, clean(task.body))
+        }
+    end
+
+    def to_ics
+      buf = []
+
+      buf << 'BEGIN:VCALENDAR'
+      buf << 'VERSION:2.0'
+      buf << 'CALSCALE:GREGORIAN'
+      buf << 'METHOD:PUBLISH'
+      buf << 'X-WR-CALNAME:Markdo Due Dates'
+      buf << events.map { |event| event.to_ics }
+      buf << 'END:VCALENDAR'
+
+      buf.flatten.join("\n")
+    end
+
+    private
+
+    def clean(line)
+      line.sub(%r(@due\(\d{4}-\d{2}-\d{2}\b\)\s), '')
+    end
+
+    class Event
+      def initialize(date_start, date_end, summary)
+        @date_start = date_start
+        @date_end = date_end
+        @summary = summary
+      end
+
+      def to_ics
+        buf = []
+        buf << 'BEGIN:VEVENT'
+        buf << "DTSTART;VALUE=DATE:#{@date_start.strftime('%Y%m%d')}"
+        buf << "DTEND;VALUE=DATE:#{@date_end.strftime('%Y%m%d')}"
+        buf << "SUMMARY:#{@summary}"
+        buf << 'END:VEVENT'
+        buf.join("\n")
+      end
+    end
+  end
+end
