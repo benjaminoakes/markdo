cce730a76e0232a3998a36151159730e8066198c
Benjamin Oakes <hello@benjaminoakes.com>
2016-09-26 20:51:19 -0500

    Refactor: Convert test to spec (SummaryCommand)

--- spec/lib/commands/summary_command_spec.rb
+++ spec/lib/commands/summary_command_spec.rb
@@ -1,10 +1,12 @@
-require 'test_helper'
+require 'spec_helper'
 require 'stringio'
 require 'markdo/commands/summary_command'
 
 module Markdo
   describe SummaryCommand do
     it 'outputs summary counts' do
+      skip 'Dir.glob not supported' unless Dir.respond_to?(:glob)
+
       out = StringIO.new
       err = StringIO.new
       env = {
@@ -14,7 +16,7 @@ module Markdo
 
       SummaryCommand.new(out, err, env, Date.new(2016, 2, 28)).run
 
-      out.string.must_equal <<-XML
+      expect(out.string).to eq(<<-XML)
 Overdue: 2
 Starred: 2
 Today: 2
