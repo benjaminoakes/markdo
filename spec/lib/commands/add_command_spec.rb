564488c0ea8dfb476a60e73879af1177403c5ad3
Benjamin Oakes <hello@benjaminoakes.com>
2016-09-27 08:07:21 -0500

    Refactor: extract before (skip) and methods clear_inbox, markdo_add, read_inbox

--- spec/lib/commands/add_command_spec.rb
+++ spec/lib/commands/add_command_spec.rb
@@ -4,70 +4,56 @@ require 'markdo/commands/add_command'
 
 module Markdo
   describe AddCommand do
-    it 'appends to the inbox' do
+    before do
       skip 'Could not access fixtures' unless File.exists?('spec/fixtures/')
+    end
 
+    it 'appends to the inbox' do
+      clear_inbox
+      markdo_add 'Example Task'
+      expect(read_inbox).to eq("- [ ] Example Task\n")
+    end
+
+    describe 'given a nil' do
+      it 'appends to the inbox' do
+        clear_inbox
+        markdo_add nil
+        expect(read_inbox).to eq('')
+      end
+    end
+
+    describe 'given an empty string' do
+      it 'appends to the inbox' do
+        clear_inbox
+        markdo_add ''
+        expect(read_inbox).to eq('')
+      end
+    end
+
+    describe 'given a blank string' do
+      it 'appends to the inbox' do
+        clear_inbox
+        markdo_add ' '
+        expect(read_inbox).to eq('')
+      end
+    end
+
+    def clear_inbox
+      File.write('spec/fixtures/add_command/inbox.md', '')
+    end
+
+    def read_inbox
+      File.read('spec/fixtures/add_command/inbox.md')
+    end
+
+    def markdo_add(task_body)
       out, err = build_command_support
       env = {
         'MARKDO_ROOT' => 'spec/fixtures/add_command',
         'MARKDO_INBOX' => 'inbox.md',
       }
 
-      File.write('spec/fixtures/add_command/inbox.md', '')
-      AddCommand.new(out, err, env).run('Example Task')
-      content = File.read('spec/fixtures/add_command/inbox.md')
-      expect(content).to eq("- [ ] Example Task\n")
-    end
-
-    describe 'given a nil' do
-      it 'appends to the inbox' do
-        skip 'Could not access fixtures' unless File.exists?('spec/fixtures/')
-
-        out, err = build_command_support
-        env = {
-          'MARKDO_ROOT' => 'spec/fixtures/add_command',
-          'MARKDO_INBOX' => 'inbox.md',
-        }
-
-        File.write('spec/fixtures/add_command/inbox.md', '')
-        AddCommand.new(out, err, env).run(nil)
-        content = File.read('spec/fixtures/add_command/inbox.md')
-        expect(content).to eq('')
-      end
-    end
-
-    describe 'given an empty string' do
-      it 'appends to the inbox' do
-        skip 'Could not access fixtures' unless File.exists?('spec/fixtures/')
-
-        out, err = build_command_support
-        env = {
-          'MARKDO_ROOT' => 'spec/fixtures/add_command',
-          'MARKDO_INBOX' => 'inbox.md',
-        }
-
-        File.write('spec/fixtures/add_command/inbox.md', '')
-        AddCommand.new(out, err, env).run('')
-        content = File.read('spec/fixtures/add_command/inbox.md')
-        expect(content).to eq('')
-      end
-    end
-
-    describe 'given a blank string' do
-      it 'appends to the inbox' do
-        skip 'Could not access fixtures' unless File.exists?('spec/fixtures/')
-
-        out, err = build_command_support
-        env = {
-          'MARKDO_ROOT' => 'spec/fixtures/add_command',
-          'MARKDO_INBOX' => 'inbox.md',
-        }
-
-        File.write('spec/fixtures/add_command/inbox.md', '')
-        AddCommand.new(out, err, env).run(' ')
-        content = File.read('spec/fixtures/add_command/inbox.md')
-        expect(content).to eq('')
-      end
+      AddCommand.new(out, err, env).run(task_body)
     end
   end
 end
