fc2d79371453763efa43fb108b23d5714d54a8d2
Benjamin Oakes <hello@benjaminoakes.com>
2016-09-28 08:25:05 -0500

    Refactor: use TaskCollection (OverviewCommand)

--- lib/markdo/commands/overview_command.rb
+++ lib/markdo/commands/overview_command.rb
@@ -1,16 +1,15 @@
 require 'markdo/commands/command'
-require 'markdo/commands/overdue_command'
-require 'markdo/commands/star_command'
-require 'markdo/commands/today_command'
-require 'markdo/commands/tomorrow_command'
 
 module Markdo
   class OverviewCommand < Command
     def run
-      commands = [OverdueCommand, StarCommand, TodayCommand, TomorrowCommand]
-      
-      commands.each do |command|
-        command.new(@stdout, @stderr, @env, @reference_date).run
+      tasks = task_collection.overdue +
+        task_collection.starred +
+        task_collection.due_today +
+        task_collection.due_tomorrow
+
+      tasks.each do |task|
+        @stdout.puts(task.line)
       end
     end
   end
