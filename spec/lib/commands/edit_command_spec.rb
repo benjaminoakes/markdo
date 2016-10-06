a11363cf13c58fb09d2a9e8e83a2e2293e190c41
Benjamin Oakes <hello@benjaminoakes.com>
2016-10-06 17:32:38 -0500

    Refactor: Replace data clump with class (in EditCommand)

--- spec/lib/commands/edit_command_spec.rb
+++ spec/lib/commands/edit_command_spec.rb
@@ -6,23 +6,27 @@ module Markdo
     it 'edits the Markdo root in your preferred editor' do
       skip 'Shellwords not supported' unless defined?(Shellwords)
 
-      out, err = build_command_support
-      env = { 'EDITOR' => 'aneditor', 'MARKDO_ROOT' => 'spec/fixtures/' }
+      command_support = build_command_support_object({
+        'EDITOR' => 'aneditor',
+        'MARKDO_ROOT' => 'spec/fixtures/'
+      })
 
       expect(Kernel).to receive(:system).with('aneditor spec/fixtures/')
 
-      EditCommand.new(out, err, env).run
+      EditCommand.new(command_support).run
     end
 
     it 'does not allow unsafe values for MARKDO_ROOT' do
       skip 'Shellwords not supported' unless defined?(Shellwords)
 
-      out, err = build_command_support
-      env = { 'EDITOR' => 'aneditor', 'MARKDO_ROOT' => '`ruin everything`' }
+      command_support = build_command_support_object({
+        'EDITOR' => 'aneditor',
+        'MARKDO_ROOT' => '`ruin everything`'
+      })
 
       expect(Kernel).to receive(:system).with('aneditor \`ruin\ everything\`')
 
-      EditCommand.new(out, err, env).run
+      EditCommand.new(command_support).run
     end
   end
 end
