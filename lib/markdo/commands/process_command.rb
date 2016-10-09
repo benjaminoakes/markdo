56e524647476162bc392bd3cc7047b4129d624d1
Benjamin Oakes <hello@benjaminoakes.com>
2016-10-09 14:40:50 -0500

    Refactor: Extract instance variable (@lines_by_filename)

--- lib/markdo/commands/process_command.rb
+++ lib/markdo/commands/process_command.rb
@@ -3,9 +3,13 @@ require 'markdo/commands/command'
 
 module Markdo
   class ProcessCommand < Command
+    def initialize(*)
+      @lines_by_filename = Hash.new { [] }
+      super
+    end
+
     def run
       lines = File.readlines(data_source.inbox_path)
-      lines_by_filename = Hash.new { [] }
 
       index = 0
 
@@ -29,16 +33,16 @@ module Markdo
           when 'h'
             HelpSubcommand.new(@command_support).run
           when 'i'
-            lines_by_filename['Inbox.md'] <<= line
+            @lines_by_filename['Inbox.md'] <<= line
             index += 1
           when 's'
-            lines_by_filename['Sprint.md'] <<= line
+            @lines_by_filename['Sprint.md'] <<= line
             index += 1
           when 'b'
-            lines_by_filename['Backlog.md'] <<= line
+            @lines_by_filename['Backlog.md'] <<= line
             index += 1
           when 'm'
-            lines_by_filename['Maybe.md'] <<= line
+            @lines_by_filename['Maybe.md'] <<= line
             index += 1
           when 'a'
             throw :abort
@@ -46,10 +50,10 @@ module Markdo
         end
 
         date = @today.iso8601
-        inbox_lines = lines_by_filename.delete('Inbox.md')
+        inbox_lines = @lines_by_filename.delete('Inbox.md')
         File.write(data_source.inbox_path, inbox_lines ? inbox_lines.join : '')
 
-        lines_by_filename.each do |filename, lines|
+        @lines_by_filename.each do |filename, lines|
           path = file_path(filename)
           new_content = ["\n## Processed on #{date}\n\n"] << lines
 
