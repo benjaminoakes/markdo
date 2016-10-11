2e44f430f49159686461b9a6a33d42c278ead07b
Benjamin Oakes <hello@benjaminoakes.com>
2016-10-10 19:44:29 -0500

    Web UI: load Markdown from data/__all__.md

--- lib/markdo_client.rb
+++ lib/markdo_client.rb
@@ -8,27 +8,28 @@ require 'markdo/models/task_collection'
 module Markdo
   class Client
     def run
-      lines = fetch_lines
-      task_collection = TaskCollection.new(lines)
+      fetch_lines.then do |lines|
+        task_collection = TaskCollection.new(lines)
 
-      attach_filter('#rb-all-count', task_collection.all)
+        attach_filter('#rb-all-count', task_collection.all)
 
-      attach_filter('#rb-overdue-count', task_collection.overdue)
-      attach_filter('#rb-due-today-count', task_collection.due_today)
-      attach_filter('#rb-wip-count', task_collection.with_tag('wip'))
-      attach_filter('#rb-starred-count', task_collection.starred)
+        attach_filter('#rb-overdue-count', task_collection.overdue)
+        attach_filter('#rb-due-today-count', task_collection.due_today)
+        attach_filter('#rb-wip-count', task_collection.with_tag('wip'))
+        attach_filter('#rb-starred-count', task_collection.starred)
 
-      attach_filter('#rb-due-tomorrow-count', task_collection.due_tomorrow)
-      attach_filter('#rb-due-soon-count', task_collection.due_soon)
-      attach_filter('#rb-deferred-until-today-count', task_collection.deferred_until_today)
-      attach_filter('#rb-next-count', task_collection.with_tag('next'))
+        attach_filter('#rb-due-tomorrow-count', task_collection.due_tomorrow)
+        attach_filter('#rb-due-soon-count', task_collection.due_soon)
+        attach_filter('#rb-deferred-until-today-count', task_collection.deferred_until_today)
+        attach_filter('#rb-next-count', task_collection.with_tag('next'))
 
-      Element['#rb-back-button'].on(:click) do
-        Element['#rb-back-button'].add_class('hidden-xs')
-        Element['#rb-markdown-document'].add_class('hidden-xs')
-        Element['#rb-markdown-document'].html = ''
-        Element['#rb-filter-nav li'].remove_class('active')
-        Element['#rb-filter-nav'].remove_class('hidden-xs')
+        Element['#rb-back-button'].on(:click) do
+          Element['#rb-back-button'].add_class('hidden-xs')
+          Element['#rb-markdown-document'].add_class('hidden-xs')
+          Element['#rb-markdown-document'].html = ''
+          Element['#rb-filter-nav li'].remove_class('active')
+          Element['#rb-filter-nav'].remove_class('hidden-xs')
+        end
       end
     end
 
@@ -55,6 +56,24 @@ module Markdo
     end
 
     def fetch_lines
+      cache_breaker = Time.now.to_i
+      promise = Promise.new
+
+      HTTP.get("data/__all__.md?#{cache_breaker}") do |response|
+        markdown = response.body
+
+        if markdown.nil?
+          promise.resolve(example_lines)
+        else
+          lines = markdown.split("\n")
+          promise.resolve(lines)
+        end
+      end
+
+      promise
+    end
+
+    def example_lines
       [
         '- [x] Example @star',
         '- [ ] Example @star',
@@ -71,11 +90,11 @@ module Markdo
 
     def render_tasks(tasks)
       lines = tasks.map { |task| task.line }
-      render_markdown(lines)
+      render_markdown(lines.join("\n"))
     end
 
-    def render_markdown(lines)
-      html = MarkdownRenderer.new(lines.join("\n")).to_html
+    def render_markdown(markdown)
+      html = MarkdownRenderer.new(markdown).to_html
       Element['#rb-markdown-document'].html = html
     end
   end
