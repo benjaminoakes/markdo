eeaf48da347e3c1dec247358cd38811c9555846e
Benjamin Oakes <hello@benjaminoakes.com>
2016-10-13 08:19:20 -0500

    Web UI: Check each filter for content

--- spec/features/filter_spec.rb
+++ spec/features/filter_spec.rb
@@ -17,11 +17,34 @@ describe 'the task filters', type: :feature do
     expect(page).to have_content 'Markdo'
   end
 
-  it 'renders loaded Markdown content' do
+  it 'renders loaded Markdown content for each filter' do
+    example_tasks = [
+      '- [x] Complete @star',
+      '- [ ] Incomplete and starred @star',
+      '- [ ] Work in Progress @wip',
+      '- [ ] Deferred @defer(2016-10-01)',
+      '- [ ] Clearly overdue @due(2016-01-01)',
+      '- [ ] Want to do soon @next',
+    ]
+
+    example_tasks << [-1, 0, 1, 2].map { |date_offset|
+      adjusted_date = Date.today + date_offset
+      "- [ ] Due on date @due(#{adjusted_date.iso8601})"
+    }
+
+    example_markdown = example_tasks.flatten.join("\n")
     FileUtils.mkdir_p('/src/docs/data/')
-    File.write('/src/docs/data/__all__.md', '- [ ] Success')
+    File.write('/src/docs/data/__all__.md', example_markdown)
+
     visit '/index.html'
-    click_on 'All'
-    expect(page).to have_content 'Success'
+    filter_links = all('#rb-filter-nav a')
+
+    expect(filter_links.count).not_to be_zero
+
+    all('#rb-filter-nav a').each do |filter_link|
+      expect(filter_link.find('.badge').text).to match(/^\d+$/)
+      click_on(filter_link.text)
+      expect(find('#rb-markdown-document').text).not_to be_empty
+    end
   end
 end
